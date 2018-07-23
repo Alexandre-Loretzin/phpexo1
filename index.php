637c3b7dc3d554eff6fa43975e36038c7f252011
alex <a.loretzin@gmail.com>
2018-07-23 12:18:19 +0200

    a

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+
+echo "Hello World ! <br>";
+
+//Exercice 1
+$name = "Alex<br> <br>";
+echo $name;
+
+//Exercice 2
+$lastname = "Loretzin ";
+$firstname = "Alexandre";
+$age = 31;
+
+echo $lastname. "<br>". $firstname."<br>". $age ."<br> <br>";
+
+//Exercice 3
+$km = 1;
+echo $km ." km"."<br>";
+$km += 2;
+echo $km ." km"."<br>";
+$km = 125;
+echo $km ." km"."<br> <br>";
+
+//Exercice 4
+
+$string = "blablabla";
+$int = 111;
+$float = 1.5;
+$bool = true;
+echo $string."<br>". $int."<br>". $float."<br>". $bool."<br><br>";
+
+//Exercice 5
+
+$nombre = null;
+echo $nombre."<br>";
+$nombre = 12;
+echo $nombre."<br><br>";
+
+//Exercice 6
+
+$name = "alex";
+echo "Bonjour ".$name." comment vas tu ?<br><br>";
+
+//Exercice 7
+
+$lastname = " Loretzin ";
+$firstname = " Alexandre ";
+$age = 31;
+
+echo "Bonjour".$lastname.$firstname.",tu as ".$age." ans<br><br>";
+
+//Exercice 8
+
+$operation1 = 3 + 4;
+$operation2 = 5 * 20;
+$operation3 = 45 / 5;
+echo $operation1." <br> ".$operation2." <br>".$operation3."<br>";
+
